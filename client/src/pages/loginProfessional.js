ea3853793f0e75e636249f02f19f35c05f2445b8
Agustin <agustinricardo14@gmail.com>
2023-05-07 11:41:44 -0300

    Cambios en loginProfessional (prueba video)

--- client/src/pages/loginProfessional.js
+++ client/src/pages/loginProfessional.js
@@ -4,7 +4,10 @@ import Layout from "../components/Layout";
 export const LoginProfessional = () => {
   return (
     <Layout>
-      <div>ESTO ES PARA USTEDES ARY Y NEHU JAJAJ XD</div>
+      <div>
+        ESTO ES PARA USTEDES ARY Y NEHU JAJAJ, SE HIZO DURANTE EL VIDEO TUTORIAL
+        XD
+      </div>
     </Layout>
   );
 };
