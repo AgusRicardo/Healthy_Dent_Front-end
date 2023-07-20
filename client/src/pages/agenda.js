114fbf745b7be0917d61d5635916da7cc92546ce
Agustin Ricardo <agustinricardo14@gmail.com>
2023-07-19 22:38:46 -0300

    [Agenda - Calendario] Terminado

--- client/src/pages/agenda.js
+++ client/src/pages/agenda.js
@@ -19,41 +19,66 @@ export const Agenda = () => {
     const monthIndex = date.getMonth();
     const currentMonthName = months[monthIndex];
     setCurrentMonth(currentMonthName.toUpperCase());
+    
   }, [date]);
+
+  const handlePrevMonth = () => {
+    setDate((prevDate) => {
+      const prevMonth = prevDate.getMonth() - 1;
+      return new Date(prevDate.getFullYear(), prevMonth, today);
+    });
+  };
+  const handleNextMonth = () => {
+    setDate((prevDate) => {
+      const prevMonth = prevDate.getMonth() + 1;
+      return new Date(prevDate.getFullYear(), prevMonth, today);
+    });
+  };
+  const handleReset = () => {
+    setDate(new Date());
+  };
+
   return (
     <Layout>
         <section className="container_agenda">
           <div className="calendario style_container">
             <div className="dia">
-              <p className='dia_numero bold'>
-                {today}
-              </p>
-              <p className='text-center bold'>
+              <div className="dia_arrows_num">
+                <i className="fa-solid fa-chevron-left dia_arrow" onClick={handlePrevMonth}></i>
+                <p className='dia_numero bold'>
+                  {today}
+                </p>
+                <i className="fa-solid fa-chevron-right dia_arrow" onClick={handleNextMonth}></i>
+              </div>
+              <p className='text-center bold dia_mes'>
                 {currentMonth} - {year}
               </p>
+              <button className="btn btn-outline-primary btn_reset" onClick={handleReset} type="button">Reset</button>
             </div>
-            <div className="calendario_completo">
+              <div className="calendario_completo">
                 <Calendar
-                onChange={setDate} 
-                value={date} 
-                onViewChange={null}
-                prevLabel={null} 
-                nextLabel={null} 
-                next2Label={null} 
-                prev2Label={null}
-                showNavigation={null}
-                selectRange={false}
-                navegaciónAriaLabel={null}
-                navegaciónAriaLive={null}
-                navigationLabel={null}
-                locale={"es"}
-                returnValue={"start"}
-                showNeighboringMonth={false}
-                minDetalle={"year"}
-                tileClassName="custom-tile"
-                calendarClassName="custom-calendar"
-                />
-            </div>
+                  onChange={setDate} 
+                  value={date} 
+                  onViewChange={null}
+                  prevLabel={null} 
+                  nextLabel={null} 
+                  next2Label={null} 
+                  prev2Label={null}
+                  showNavigation={null}
+                  selectRange={false}
+                  navegaciónAriaLabel={null}
+                  navegaciónAriaLive={null}
+                  navigationLabel={({ label }) => <div>{label}</div>}
+                  locale={"es"}
+                  //returnValue={"start"}
+                  showNeighboringMonth={false}
+                  minDetalle={"year"}
+                  // onClickDay={handleDayClick}
+                  tileDisabled={() => true}
+                  tileClassName="custom-tile"
+                  calendarClassName="custom-calendar"
+                  />
+              </div>
           </div>
           <div className="ult_turnos style_container">
             Turnos del día de hoy:
