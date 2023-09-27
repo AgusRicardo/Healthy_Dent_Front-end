a99f522caa4df53a6c21d72db58e4e3ec94a2234
Agustin <agustinricardo14@gmail.com>
2023-09-27 19:57:30 -0300

    #340 - Nuevo modal Mis Pacientes

--- client/src/pages/misPacientes.js
+++ client/src/pages/misPacientes.js
@@ -28,18 +28,67 @@ export const MisPacientes = () => {
                 <tr>
                   <td>apellido, nombre</td>
                   <td>44.232.256</td>
-                  <td><i className="fa-solid fa-eye"></i></td>
+                  <td><i className="fa-solid fa-eye" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></td>
                 </tr>
                 <tr>
                   <td>apellido2, nombre2</td>
                   <td>4.552.268</td>
-                  <td><i className="fa-solid fa-eye"></i></td>
+                  <td><i className="fa-solid fa-eye" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></td>
                 </tr>
               </tbody>
             </table>
             </div>
           </div>
         </div>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog sin-padding modal-dialog-scrollable modal-dialog-centered">
+            <div class="modal-content sin-padding">
+                <div class="modal-header info_paciente_header">
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body box-modal container_body_modal">
+                  <div className="container_info_foto">
+                    <div className="container_info_paciente">
+                      <div className="container_nombre_paciente">
+                        <p className='row-title'>Paciente</p>
+                        <p>nombre apellido</p>
+                      </div>
+                      <div className="container_dni_paciente">
+                        <p className='row-title'>DNI</p>
+                        <p>400000000</p>
+                      </div>
+                      <div className="container_email_paciente">
+                        <p className='row-title'>Email</p>
+                        <p>nombreapellido@gmail.com</p>
+                      </div>
+                      <div className="container_telefono_paciente">
+                      <p className='row-title'>Teléfono</p>
+                        <p>121231231</p>
+                      </div>
+                      <div className="container_domicilio_paciente">
+                        <p className='row-title'>Domicilio</p>
+                        <p>calle falsa 123</p>
+                      </div>
+                    </div>
+                    <div className="container_foto_paciente">
+                      <div className='cont_foto_pac'>
+                        <i className='fa-solid fa-circle-user icono_modal_paciente'></i>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="container_seguimiento_paciente">
+                    <p className="row-title">Seguimiento Clínico</p>
+                    <div className='container_historial_p'>
+                      <div className="motivo">motivo visita</div>
+                      <div className="fecha_visita">fecha</div>
+                      <div className="prox_modal"><i className="fa-solid fa-eye"></i></div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
       </Layout>
     </>
   )
